f238886e3a592016c28fbde7195a0fbe80f43f1a
Korti <lucas.korthauer@gmail.com>
2020-02-27 16:28:56 +0100

    Gets the column name and put it in the output.

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
+  if(!process.env.GITHUB_TOKEN) {
+    core.setFailed('GITHUB_TOKEN missing, must be set with ${{secrets.GITHUB_TOKEN}}')
+  }
+
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const {eventName, payload} = github.context;
+    const action = payload.action;
+    const columnId = payload.project_card.column_id
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    validateEvent(eventName, action);
 
-    core.setOutput('time', new Date().toTimeString());
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN)
+    const { data: projectColumn } = await octokit.projects.getCard({
+      column_id: columnId
+    });
+
+    core.setOutput("column_name", projectColumn.name)
   } 
   catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+function validateEvent(eventName, action) {
+  if(eventName != 'project_card' || action != 'moved') {
+    throw new Error(`Only the push event is allowed, used event: ${eventName}`)
   }
 }
 
